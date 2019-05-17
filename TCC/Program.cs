46c35b9916b780e1af40fee073a315339e8a13bd
Lucas Salvino de Deus <lucassalvino1@gmail.com>
2019-05-17 19:42:05 -0300

    Add project files.

---
+++ TCC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
